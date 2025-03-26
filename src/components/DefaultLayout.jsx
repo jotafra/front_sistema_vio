765b5f7e9f8e8f9bfc4a5a7a0486ff337855d2bc
jotafra <jozinbrbrasil@gmail.com>
2025-03-26 10:47:03 -0300

    26/03

---
+++ src/components/DefaultLayout.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+import Box from "@mui/material/Box";
+
+const DefaultLayout = ({children}) =>{
+    return(
+        <Box sx={{display:"flex", flexDirection:"column", minHeight:"100px"}}>
+            <Header/>
+            {/* Aqui vem o conteudo da página  */}
+            <Box sx={{flex: 1, display: "flex", justifyContent:"center", 
+                alignItems:"center", padding: "20px"
+                }}
+            >
+                {children}
+            </Box>
+            <Footer/>
+        </Box>
+    );
+};
+
+export default DefaultLayout;
